9d0f0b5d79169fdc2bad9c9ae453dab3f1fdc511
ImTerry <154828561+terrencerusike@users.noreply.github.com>
2025-10-28 21:35:48 +0200

    dd

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,6 @@ mongoose
   .catch((err) => console.log("❌ MongoDB connection error:", err));
 
 // Middlewares
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
