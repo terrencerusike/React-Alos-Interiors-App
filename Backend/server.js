ac65cd0254caf924736d5f37c5407cc0f71453f3
ImTerry <154828561+terrencerusike@users.noreply.github.com>
2025-10-28 12:53:12 +0200

    fix

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // React routing: serve index.html for all other routes
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
